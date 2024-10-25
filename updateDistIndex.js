b045207b6b459d18da51cbbef5a5c3d6a12efe1e
ATacoDev <taco072103gmail.com>
2024-10-24 22:58:58 -0700

    Deployment bugfixing

---
+++ updateDistIndex.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+const distPath = path.resolve('dist/index.html');
+const faviconPath = './images/favicon.svg';
+const cssPath = './assets/index-DCoUxNct.css';
+const jsPath = './assets/index-BDEPgNRf.js';
+
+fs.readFile(distPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the index.html file:', err);
+        return;
+    }
+
+    let result = data
+        .replace(/<link rel="shortcut icon" href="[^"]*" type="image\/svg\+xml">/, `<link rel="shortcut icon" href="${faviconPath}" type="image/svg+xml">`)
+        .replace(/<link rel="stylesheet" href="[^"]*index-DCoUxNct\.css">/, `<link rel="stylesheet" href="${cssPath}">`)
+        .replace(/<script type="module" crossorigin src="[^"]*index-BDEPgNRf\.js"><\/script>/, `<script type="module" crossorigin src="${jsPath}"></script>`);
+    
+    fs.writeFile(distPath, result, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing the updated index.html file:', err);
+        } else {
+            console.log('Updated index.html successfully!');
+        }
+    });
+});
