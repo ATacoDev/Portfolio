af1f7208deaa22fb21a90c8a569e92b1b7d2444d
ATacoDev <taco072103gmail.com>
2024-10-24 23:05:55 -0700

    Deployment bigfix with updateDistIndex.js

--- updateDistIndex.js
+++ updateDistIndex.js
@@ -15,6 +15,7 @@ fs.readFile(distPath, 'utf8', (err, data) => {
     let result = data
         .replace(/<link rel="shortcut icon" href="[^"]*" type="image\/svg\+xml">/, `<link rel="shortcut icon" href="${faviconPath}" type="image/svg+xml">`)
         .replace(/<link rel="stylesheet" href="[^"]*index-DCoUxNct\.css">/, `<link rel="stylesheet" href="${cssPath}">`)
+        .replace(/<link rel="stylesheet" crossorigin href="[^"]*index-DCoUxNct\.css">/, '') // Remove the duplicate link
         .replace(/<script type="module" crossorigin src="[^"]*index-BDEPgNRf\.js"><\/script>/, `<script type="module" crossorigin src="${jsPath}"></script>`);
     
     fs.writeFile(distPath, result, 'utf8', (err) => {
