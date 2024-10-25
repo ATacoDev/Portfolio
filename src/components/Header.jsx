c1c1eefe0a3322d20bacd6db003278259f300956
ATacoDev <taco072103gmail.com>
2024-10-25 10:10:18 -0700

    Fixing top logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
             <h1>
                 <a 
-                href="/" 
+                href="/Portfolio/" 
                 className="logo"
                 >
                     <img src="images/taco.svg"  width={40} height={40} alt="Jaden Johnson"/>
